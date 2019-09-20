27965a944b0edc575ca99d9f73231a6988eb1d82
Jonathan Mourtada <jonathan.mourtada@divid.se>
2019-09-20 12:23:00 +0200

    remove commented code

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const demoPath = path.resolve(__dirname, "../demo_dist");
 
@@ -13,8 +12,6 @@ module.exports = {
     extensions: [".mjs", ".ts", ".tsx", ".js"]
   },
   plugins: [
-    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
-    // new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "Development"
     })
